48ca35e9884374833bb77e3fc9f62f1ef5e84cfc
omprakash8788 <omprakashkumar552@gmail.com>
2025-05-17 13:44:19 +0530

    about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          ABOUT <span className="text-gray-700 font-medium">US</span>
+        </p>
+      </div>
+      <div className="my-10 flex-col flex md:flex-row gap-12">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.about_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni,
+            aspernatur!
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil fuga
+            consectetur rerum?
+          </p>
+          <b className="text-gray-800">Our Vision</b>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
+            optio consequatur quos velit quibusdam magni.
+          </p>
+        </div>
+      </div>
+      {/* Why choose section */}
+      <div className="text-xl my-4">
+        <p>Why <span className="text-gray-700 font-semibold">Choose Us</span> </p>
+      </div>
+      {/* card */}
+      <div className="flex flex-col md:flex-row mb-2">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-blue-700 hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+           <b>Efficiency:</b>
+           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, ullam.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-blue-700 hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+           <b>Convenience:</b>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur!</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-blue-700 hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+           <b>Personalization:</b>
+           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
