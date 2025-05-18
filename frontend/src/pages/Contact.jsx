672d320a042e9e3f75bc9d4b1dfe6805f126ecbf
omprakash8788 <omprakashkumar552@gmail.com>
2025-05-18 11:33:34 +0530

    contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 const Contact = () => {
   return (
     <div>
-      Contact
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          Contact <span className="text-gray-700 font-semibold">Us</span>
+        </p>
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+        <img className="w-full md:max-w-[360px]" src={assets.contact_image} alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-lg text-gray-600">Our Office</p>
+          <p className="text-gray-500">
+            42023 CST station ,<br /> Mumbai India
+          </p>
+          <p className="text-gray-500">
+            Tel: (91) 787878787 <br /> Email:op@gmail.com
+          </p>
+          <p className="font-semibold text-lg text-gray-600">Careers at Ashoka</p>
+          <p className="text-gray-500">Learn more about our teams and job openings.</p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 cursor-pointer">Explore Jobs</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
