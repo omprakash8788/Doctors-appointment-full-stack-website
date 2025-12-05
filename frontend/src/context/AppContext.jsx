9ff1d21ee19e1431ad6e4acdd6fcae8face5aa04
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-05 23:09:11 +0530

    api:adding api

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -16,6 +16,29 @@ const AppContextProvider = (props)=>{
   // create one state variable where we can store the user authentication token
   const [token, setToken]=useState(localStorage.getItem('token') ? localStorage.getItem('token'): false);
 
+  // here we create one state where we will store user data.
+  const [userData, setUserData] = useState(false);
+  // After that create arrow functio, using that we get the user data and save the data into state variable.
+  const loadUserProfileData = async ()=>{
+
+    try {
+         const {data} = await axios.get(backendUrl + '/api/user/get-profile', {headers:{token}});
+      // {data} --> here we will store the respons info of user.
+      if(data.success){
+         setUserData(data.userData)
+      }
+      else{
+        toast.error(data.message)
+      }
+
+      
+    } catch (error) {
+       console.log(error);
+       toast.error(error.message)
+    }
+  }
+
+  
 
   // create arrow function where we will call the Api
   const getDoctorsData=async()=>{
@@ -40,6 +63,16 @@ const AppContextProvider = (props)=>{
     getDoctorsData()
   },[])
 
+  // Run the function whenever the user will be login 
+    useEffect(()=>{
+      if(token){
+        loadUserProfileData()
+      }
+      else{
+        setUserData(false)
+      }
+  },[token])
+
   // in this function we will add one variable with the "value"
   const value={
     // Whatever we add in the value obj, we can access that in any component.
@@ -47,7 +80,10 @@ const AppContextProvider = (props)=>{
     currencySymbol,
     token,
     setToken,
-    backendUrl
+    backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData
   }
   return(
     <AppContext.Provider value={value}>
