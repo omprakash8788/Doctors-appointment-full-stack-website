7db5f2f81f875f45e21595ccf38fbffc9812ac2d
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-13 00:03:51 +0530

    feat: AllAppointment Api integration

--- admin/src/pages/Admin/AllAppointment.jsx
+++ admin/src/pages/Admin/AllAppointment.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { AdminContext } from "../../context/AdminContext";
+import { useEffect } from "react";
 
 const AllAppointment = () => {
-  return (
-    <div>AllAppointment</div>
-  )
-}
+  const { aToken, appointments, setAppointments, getAllAppointments } =
+    useContext(AdminContext);
+  console.log(appointments);
 
-export default AllAppointment
+  useEffect(() => {
+    if (aToken) {
+      getAllAppointments();
+    }
+  }, [aToken]);
+
+  return <div>AllAppointment</div>;
+};
+
+export default AllAppointment;
