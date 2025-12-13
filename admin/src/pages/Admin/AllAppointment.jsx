6d921955d48c510ac48eef1011cdfe6456a2afd7
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-13 12:57:35 +0530

    feat: api inte

--- admin/src/pages/Admin/AllAppointment.jsx
+++ admin/src/pages/Admin/AllAppointment.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useContext } from "react";
 import { AdminContext } from "../../context/AdminContext";
 import { useEffect } from "react";
+import { AppContext } from "../../context/AppContext";
+import { assets } from "../../assets_admin/assets";
 
 const AllAppointment = () => {
   const { aToken, appointments, setAppointments, getAllAppointments } =
     useContext(AdminContext);
-  console.log(appointments);
+  const { calculateAge, slotDateFormat } = useContext(AppContext);
+  // console.log(appointments);
 
   useEffect(() => {
     if (aToken) {
@@ -14,7 +17,57 @@ const AllAppointment = () => {
     }
   }, [aToken]);
 
-  return <div>AllAppointment</div>;
+  return (
+    <div className="w-full max-w-6xl m-5">
+      <p className="mb-3 text-lg font-medium">All Appointments</p>
+      <div className="bg-white min-h-[60vh] border rounded text-sm max-h-[80vh] overflow-y-scroll">
+        <div className="hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b">
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Action</p>
+        </div>
+        {/* Display appointment data */}
+        {appointments.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-wrap justify-between max-sm:gap-2 sm:grid sm:grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50"
+          >
+            <p className="max-sm:hidden">{index + 1}</p>
+            <div className="flex items-center gap-2 ">
+              <img
+                className="w-8 rounded-full"
+                src={item.userData.image}
+                alt="img"
+              />
+              <p>{item.userData.name}</p>
+            </div>
+            <p className="max-sm:hidden">{calculateAge(item.userData.dob)}</p>
+            <p>
+              {slotDateFormat(item.slotDate)}, {item.slotTime}
+            </p>
+            <div className="flex items-center gap-2 ">
+              <img
+                className="w-8 rounded-full bg-amber-400"
+                src={item.docData.image}
+                alt="img"
+              />
+              <p>{item.docData.name}</p>
+            </div>
+            <p>$ {item.docData.fees}</p>
+            <img
+              className="w-10 cursor-pointer"
+              src={assets.cancel_icon}
+              alt="can"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllAppointment;
