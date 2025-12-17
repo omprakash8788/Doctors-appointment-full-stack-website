2f49c42d234ba531a7f23a16369ba480dcf28b50
omprakash kumar <112754995+omprakash8788@users.noreply.github.com>
2025-12-17 08:25:20 +0530

    Update Doctors.jsx

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext';
 
-
 const Doctors = () => {
   const [showFilter, setShowFilter]=useState(false)
 
