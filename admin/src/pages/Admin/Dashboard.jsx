48a90b5b5fa97a64e62cebcb6e5a416f6ea0b627
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-19 12:49:42 +0530

    dashboard

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -3,10 +3,13 @@ import { useContext } from "react";
 import { AdminContext } from "../../context/AdminContext";
 import { useEffect } from "react";
 import { assets } from "../../assets_admin/assets";
+import { AppContext } from "../../context/AppContext";
 
 const Dashboard = () => {
   const { aToken, getDashData, dashData, cancelAppointment } =
     useContext(AdminContext);
+    const {slotDateFormat } = useContext(AppContext);
+
   console.log(dashData);
 
   useEffect(() => {
@@ -49,7 +52,34 @@ const Dashboard = () => {
               <p className="text-gray-400">Patients</p>
             </div>
           </div>
-          {/*  */}
+        </div>
+        {/* display latest appointment */}
+        <div className="bg-white">
+          <div className="flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border">
+            <img src={assets.list_icon} alt="icon" />
+            <p className="font-semibold">Latest Booking</p>
+          </div>
+          <div className="pt-4 border border-t-0">
+            {dashData.latestAppointments.map((item, index) => (
+              <div className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100" key={index}>
+                <img className="rounded-full w-10" src={item.docData.image} alt="img" />
+                <div className="flex-1 text-sm">
+                  <p className="text-gray-800 font-medium">{item.docData.name}</p>
+                  <p className="text-gray-600">{slotDateFormat(item.slotDate)}</p>
+                </div>
+                {item.cancelled ? (
+                  <p className="text-red-400 text-xs font-medium">Cancelled</p>
+                ) : (
+                  <img
+                    onClick={() => cancelAppointment(item._id)}
+                    className="w-10 cursor-pointer"
+                    src={assets.cancel_icon}
+                    alt="can"
+                  />
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     )
