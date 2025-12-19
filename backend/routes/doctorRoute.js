644f14ca244f735d1be6954a18f02705f7312caa
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-19 13:57:21 +0530

    feat:docApi end point

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,10 +1,11 @@
 // import express from express;
 import express from "express";
-import { doctorList } from "../controllers/doctorController.js";
+import { doctorList, loginDoctor } from "../controllers/doctorController.js";
 //create router instance 
 const doctorRouter = express.Router();
 // using doctorRouter we will create multiple routes
 // Endponit for get all doctor list
 doctorRouter.get('/list', doctorList)
+doctorRouter.post('/login', loginDoctor)
 // After that export the doctorRouter
 export default doctorRouter;
