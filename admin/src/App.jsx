b2a93eaf0174b42173a0bd8fc65bf5b876dfff31
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-19 16:04:53 +0530

    feat:docApi end point

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import Login from './pages/Login'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
@@ -10,11 +10,13 @@ import Dashboard from './pages/Admin/Dashboard';
 import AllAppointment from './pages/Admin/AllAppointment';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorsList from './pages/Admin/DoctorsList';
+import { DoctorContext } from './context/DoctorContext';
 const App = () => {
   const {aToken}=useContext(AdminContext)
   console.log(aToken)
+  const {dToken}=useContext(DoctorContext)
 
-  return aToken ?  (
+  return aToken || dToken ?  (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
       <Navbar/>
