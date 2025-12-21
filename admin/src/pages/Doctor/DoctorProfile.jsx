b50294e1dfe1f1249d3c86961b84939fc3c02e15
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-21 11:44:00 +0530

    api int

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -3,12 +3,17 @@ import { useContext } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
 import { AppContext } from "../../context/AppContext";
 import { useEffect } from "react";
+import { useState } from "react";
 
 const DoctorProfile = () => {
   const { profileData, setProfileData, getProfileData, dToken } =
     useContext(DoctorContext);
   const { backendUrl } = useContext(AppContext);
   console.log(profileData);
+ 
+  const [isEdit, setIsEdit] = useState(false);
+
+
   useEffect(() => {
     if (dToken) {
       getProfileData();
@@ -19,39 +24,67 @@ const DoctorProfile = () => {
       <div>
         <div className="flex flex-col gap-4 m-5">
           <div>
-            <img className="bg-purple-600 w-full sm:max-w-64 rounded-lg" src={profileData.image} alt="" />
+            <img
+              className="bg-purple-600 w-full sm:max-w-64 rounded-lg"
+              src={profileData.image}
+              alt=""
+            />
           </div>
           <div className="flex-1 border border-stone-100 rounded-lg p-8 py-7 bg-gray-200">
             {/* doc info: name, degree, experiance */}
-            <p className="flex items-center gap-2 text-3xl font-medium text-gray-700">{profileData.name}</p>
+            <p className="flex items-center gap-2 text-3xl font-medium text-gray-700">
+              {profileData.name}
+            </p>
             <div className="flex items-center gap-2 mt-1 text-gray-600">
               <p>
                 {profileData.degree} - {profileData.speciality}
               </p>
-              <button className="py-0.5 px-2 border text-xs rounded-full">{profileData.experience}</button>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {profileData.experience}
+              </button>
             </div>
             {/* doc about */}
             <div>
-              <p className="flex items-center gap-1 text-sm font-medium text-neutral-800 mt-3">About:</p>
-              <p className="text-sm text-gray-600 max-w-[700px] mt-1">{profileData.about}</p>
+              <p className="flex items-center gap-1 text-sm font-medium text-neutral-800 mt-3">
+                About:
+              </p>
+              <p className="text-sm text-gray-600 max-w-[700px] mt-1">
+                {profileData.about}
+              </p>
             </div>
             <p className="text-gray-600 font-medium mt-4">
-              Appointment fees: <span className="text-gray-800">$ {profileData.fees}</span>
+              Appointment fees:{" "}
+              <span className="text-gray-800">$ {isEdit ? <input type="number" onChange={(e)=>setProfileData(prev => ({...prev, fees:e.target.value}))} value={profileData.fees}/> :  profileData.fees}</span>
             </p>
             <div className="flex gap-2 py-2">
               <p>Address:</p>
               <p className="text-sm text-gray-500">
-                {profileData.address.line1} <br />
-                {profileData.address.line2}
+                {isEdit ? <input type="text" onChange={(e)=>setProfileData(prev=> ({...prev, address:{...prev.address, line1:e.target.value}}))} value={profileData.address.line1}/> : profileData.address.line1}
+                 <br />
+                {/* {profileData.address.line2} */}
+                   {isEdit ? <input type="text" onChange={(e)=>setProfileData(prev=> ({...prev, address:{...prev.address, line2:e.target.value}}))} value={profileData.address.line2}/> : profileData.address.line2}
               </p>
             </div>
             {/* ano div */}
             <div className="flex gap-1 pt-2">
-              <input type="checkbox" />
+              <input onChange={()=>isEdit && setProfileData(prev=>({...prev, available:!prev.available}))} checked={profileData.available} type="checkbox" />
               <label htmlFor="">Available</label>
             </div>
             {/* ano */}
-            <button className="px-4 py-1 border border-purple-600 text-sm rounded-full mt-5 hover:bg-purple-500 hover:text-white transition-all">Edit</button>
+            {
+              isEdit ? 
+                 <button onClick={()=>setIsEdit(false)} className="px-4 py-1 border border-purple-600 text-sm rounded-full mt-5 hover:bg-purple-500 hover:text-white transition-all">
+              Save
+            </button>
+            : 
+               <button onClick={()=>setIsEdit(true)} className="px-4 py-1 border border-purple-600 text-sm rounded-full mt-5 hover:bg-purple-500 hover:text-white transition-all">
+              Edit
+            </button>
+            }
+         
+
+          
+
           </div>
         </div>
       </div>
