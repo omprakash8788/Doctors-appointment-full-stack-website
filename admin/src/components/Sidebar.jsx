b61d5aef56729c085a85c337f705ff3b0bf3452a
omprakash8788 <omprakashkumar552@gmail.com>
2025-12-22 10:03:49 +0530

    bug fixing

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -5,17 +5,12 @@ import { assets } from "../assets_admin/assets";
 import { DoctorContext } from "../context/DoctorContext";
 
 const Sidebar = () => {
-  // First import aToken from the Admin context.
   const { aToken } = useContext(AdminContext);
   const {dToken}=useContext(DoctorContext)
-  // After that in div add ternary operator
   return (
     <div className="min-h-screen bg-white border-r">
-      {/* here we will check if we have AToken in that case we will return one UL tag */}
       {aToken && (
         <ul className="text-[#515151] mt-5">
-          {/* in this ul tage we will create sidebar */}
-          {/* in navlink we are using dynamic classname- inside obj{} we pass 'isActive' we are getting inside obj. so if our navlink is active it means the "isActive" will be true and if this navlink not active then "isActive" property will be false */}
           <NavLink
             className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
@@ -64,11 +59,8 @@ const Sidebar = () => {
       )}
 
       {/* for doctor */}
-        {/* here we will check if we have AToken in that case we will return one UL tag */}
       {dToken && (
         <ul className="text-[#515151] mt-5">
-          {/* in this ul tage we will create sidebar */}
-          {/* in navlink we are using dynamic classname- inside obj{} we pass 'isActive' we are getting inside obj. so if our navlink is active it means the "isActive" will be true and if this navlink not active then "isActive" property will be false */}
           <NavLink
             className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
